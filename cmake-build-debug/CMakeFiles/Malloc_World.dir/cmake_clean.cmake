b8f68955a6e4fb24995900a999e87aa10eb67317
Oniizukaa <daoudhandas@gmail.com>
2021-11-19 23:54:07 +0100

    Clean Version

---
+++ cmake-build-debug/CMakeFiles/Malloc_World.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Malloc_World.dir/main.c.obj"
+  "Malloc_World.exe"
+  "Malloc_World.exe.manifest"
+  "Malloc_World.pdb"
+  "libMalloc_World.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Malloc_World.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
